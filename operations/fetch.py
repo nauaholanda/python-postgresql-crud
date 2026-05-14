8330bf8f37c102a4bf5109e73e55893eb382ea2a
Nauã Holanda <nauasholanda@gmail.com>
2026-05-14 16:21:32 -0300

    feat: creating fetch operation

---
+++ operations/fetch.py
@@ -0,0 +1,17 @@
+import psycopg2
+
+def fetch_books():
+  conn = psycopg2.connect("dbname=teste_python user=postgres password=password")
+  cursor = conn.cursor()
+
+  cursor.execute('SELECT id, nome, autor, avaliacao FROM livros ORDER BY id')
+
+  print(f'Temos {cursor.rowcount} livros cadastrados na nossa base:')
+
+  books = cursor.fetchall()
+
+  for book in books:
+    print(f'{book[0]} - {book[1]} de {book[2]} - Nota: {book[3]}/10.0')
+
+  cursor.close()
+  conn.close()
