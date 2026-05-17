2d006fee1cb281006152976a3a48353eb96024ed
Nauã Holanda <nauasholanda@gmail.com>
2026-05-17 14:44:46 -0300

    feat: create Book class

--- entities/__init__.py
+++ entities/__init__.py
@@ -1,1 +1,2 @@
 from .base import Base
+from .book import Book
---
+++ entities/book.py
@@ -0,0 +1,13 @@
+from typing import Optional
+from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy import Float
+from entities import Base
+
+class Book(Base):
+  __tablename__ = 'books'
+  
+  id: Mapped[int] = mapped_column(primary_key=True)
+  title: Mapped[str] = mapped_column(nullable=False)
+  author: Mapped[str] = mapped_column(nullable=False)
+  review_points: Mapped[Optional[float]] = mapped_column(Float(precision=1, max=10))
+  
