6aaf921af3d312d371ecd314e6b873ff26c0f897
Nauã Holanda <nauasholanda@gmail.com>
2026-05-18 21:56:55 -0300

    feat: add reviews table creation to db_setup.sql

--- db_setup.sql
+++ db_setup.sql
@@ -4,3 +4,10 @@ CREATE TABLE books (
   author VARCHAR(255) NOT NULL,
   rating FLOAT CHECK (rating <= 10)
 );
+
+CREATE TABLE reviews (
+  id SERIAL PRIMARY KEY,
+  book_id INTEGER REFERENCES books(id) ON DELETE CASCADE,
+  review_text TEXT NOT NULL,
+  reviewer_name VARCHAR(255) NOT NULL
+);
