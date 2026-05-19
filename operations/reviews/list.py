09ea35a848f802916901a4c37578831c62525915
Nauã Holanda <nauasholanda@gmail.com>
2026-05-19 14:42:23 -0300

    feat: create list review function

---
+++ operations/reviews/list.py
@@ -0,0 +1,17 @@
+from operations.books import fetch_books
+from repositories import ReviewRepository
+
+def list_reviews():
+  fetch_books()
+
+  try:
+    book_id = int(input('\nEnter the ID of the book you want to list the reviews: '))
+
+    review_repository = ReviewRepository()
+    reviews = review_repository.find_all_by_book_id(book_id)
+
+    for review in reviews:
+      print(f"\nReviewer: {review.reviewer_name}\n{'-'*20}\n{review.review_text}")
+
+  except Exception as err:
+    print(f'Error listing reviews: {err}. Returning to Menu...')
