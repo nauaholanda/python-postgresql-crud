0e6b5f0d1a849f56b010a855d4705528e6f58b77
Nauã Holanda <nauasholanda@gmail.com>
2026-05-19 14:44:46 -0300

    feat: create insert review function on review repository

--- repositories/review_repository.py
+++ repositories/review_repository.py
@@ -6,3 +6,9 @@ class ReviewRepository:
     with DBConnection() as db:
       book_reviews = db.session.query(Review).filter(Review.book_id == book_id).all()
       return book_reviews
+  
+  def insert(self, review):
+    with DBConnection() as db:
+      db.session.add(review)
+      db.session.commit()
+      
