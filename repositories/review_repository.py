350cc087434b0d872f18f0f42f1d076ed4431f84
Nauã Holanda <nauasholanda@gmail.com>
2026-05-19 14:41:06 -0300

    fix: add self parameter to review repository function

--- repositories/review_repository.py
+++ repositories/review_repository.py
@@ -2,7 +2,7 @@ from configs import DBConnection
 from entities import Review
 
 class ReviewRepository:
-  def find_all_by_book_id(book_id):
+  def find_all_by_book_id(self, book_id):
     with DBConnection() as db:
       book_reviews = db.session.query(Review).filter(Review.book_id == book_id).all()
       return book_reviews
