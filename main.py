40bec9750edcea327ca25cbb7732d5786198ad6b
Nauã Holanda <nauasholanda@gmail.com>
2026-05-19 14:42:52 -0300

    feat: add list reviews function to main script

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from operations.books import fetch_books, insert_book, update_book, delete_book
+from operations.reviews import list_reviews
 
 def main():
     print("Welcome to the book registry!")
@@ -8,6 +9,7 @@ def main():
         print("2 - Register a new book")
         print("3 - Edit an existing book")
         print("4 - Delete a book")
+        print("5 - List a book's reviews")
         print("(type 999 or press Ctrl+C to exit)")
         try:
             action_number = int(input("What would you like to do? (enter the action number): "))
@@ -26,6 +28,8 @@ def main():
                     update_book()
                 case 4:
                     delete_book()
+                case 5:
+                    list_reviews()
                 case _:
                     print("Unknown action")
         
