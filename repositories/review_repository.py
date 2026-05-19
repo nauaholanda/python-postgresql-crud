c2a03753bd9751cd992bedf549b1d2e799de4ba3
Nauã Holanda <nauasholanda@gmail.com>
2026-05-19 14:18:27 -0300

    feat: create function to find all review by book id

--- repositories/review_repository.py
+++ repositories/review_repository.py
@@ -1,2 +1,8 @@
+from configs import DBConnection
+from entities import Review
+
 class ReviewRepository:
-  pass
+  def find_all_by_book_id(book_id):
+    with DBConnection() as db:
+      book_reviews = db.session.query(Review).filter(Review.book_id == book_id).all()
+      return book_reviews
