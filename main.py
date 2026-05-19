ed91fe261126915df850ee9686aa50c69f6e66b0
Nauã Holanda <nauasholanda@gmail.com>
2026-05-19 15:09:18 -0300

    feat: add insert review action to main script

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from operations.books import fetch_books, insert_book, update_book, delete_book
-from operations.reviews import list_reviews
+from operations.reviews import list_reviews, insert_review
 
 def main():
     print("Welcome to the book registry!")
@@ -10,6 +10,7 @@ def main():
         print("3 - Edit an existing book")
         print("4 - Delete a book")
         print("5 - List the reviews of a book")
+        print("6 - Insert a review of a book")
         print("(type 999 or press Ctrl+C to exit)")
         try:
             action_number = int(input("What would you like to do? (enter the action number): "))
@@ -30,6 +31,8 @@ def main():
                     delete_book()
                 case 5:
                     list_reviews()
+                case 6:
+                    insert_review()
                 case _:
                     print("Unknown action")
         
