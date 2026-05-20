f46b9af2ade5d67575714c630a4ed0d3df193f94
Nauã Holanda <nauasholanda@gmail.com>
2026-05-20 18:24:44 -0300

    feat: add author validation to insert book operation

--- operations/books/insert.py
+++ operations/books/insert.py
@@ -6,6 +6,7 @@ def insert_book():
     title = input("Enter the title of the new book: ")
     if not title: raise ValueError('The book title is required!')
     author = input("Enter the author of this book: ")
+    if not title: raise ValueError('The book author is required!')
     rating = float(input("Enter the rating for this book: "))
 
     book_repository = BookRepository()
