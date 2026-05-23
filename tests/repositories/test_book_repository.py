05d4e8325d14db3340ffee6f95d4dffef5c7694a
Nauã Holanda <nauasholanda@gmail.com>
2026-05-22 21:15:58 -0300

    test: create select all books successfully test case

--- tests/repositories/test_book_repository.py
+++ tests/repositories/test_book_repository.py
@@ -1,7 +1,8 @@
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 import pytest
 
 from src.repositories.book_repository import BookRepository
+from src.entities.book import Book
 
 @pytest.fixture
 def book_repository():
@@ -13,3 +14,17 @@ def mock_db_session():
   mock_session = MagicMock()
   mock_db.session = mock_session
   return mock_db, mock_session
+
+class TestSelectBooks:
+  def test_return_all_books_successfully(self, book_repository: BookRepository, mock_db_session):
+    mock_db, mock_session = mock_db_session
+    expected = [MagicMock(spec=Book), MagicMock(spec=Book)]
+
+    mock_session.query.return_value.all.return_value = expected
+
+    with patch("src.repositories.book_repository.DBConnection") as MockDBConnection:
+      MockDBConnection.return_value.__enter__.return_value = mock_db
+
+      response = book_repository.select()
+
+    assert response == expected
