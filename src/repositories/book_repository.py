754753d839f37ea51adc8c4809b08306933ee761
Nauã Holanda <nauasholanda@gmail.com>
2026-05-23 19:37:50 -0300

    refactor: adapt update book function in repository to attend Rest API patterns

--- src/repositories/book_repository.py
+++ src/repositories/book_repository.py
@@ -1,5 +1,6 @@
 from configs import DBConnection
 from entities import Book
+from exceptions import BookNotFoundException
 from sqlalchemy.orm.exc import NoResultFound
 
 class BookRepository:
@@ -34,15 +35,19 @@ class BookRepository:
         db.session.rollback()
         raise exception
 
-  def update(self, book: Book):
+  def update(self, book_id: int, update_data: dict) -> Book | None:
     with DBConnection() as db:
       try:
-        db.session.query(Book).filter(Book.id == book.id).update({
-          'title': book.title,
-          'author': book.author,
-          'rating': book.rating
-        })
+        rows_updated = db.session.query(Book).filter(Book.id == book_id).update(update_data)
+        
+        if rows_updated == 0:
+            raise BookNotFoundException(f"Book with ID {book_id} not found.")
+        
         db.session.commit()
+        
+        updated_book = db.session.query(Book).filter(Book.id == book_id).first()
+        return updated_book
+          
       except Exception as exception:
         db.session.rollback()
         raise exception
