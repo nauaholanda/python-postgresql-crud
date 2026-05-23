a642a1945eda7b0f875d72d784acfd6f0b9b519c
Nauã Holanda <nauasholanda@gmail.com>
2026-05-23 19:09:57 -0300

    feat: add create book function in book service

--- src/services/book_service.py
+++ src/services/book_service.py
@@ -1,8 +1,15 @@
 from repositories import BookRepository
-from schemas.book import BookResponse
+from schemas.book import BookResponse, BookCreate
+from entities import Book
 
 def get_all() -> list[BookResponse]:
   book_repository = BookRepository()
   books = book_repository.select()
   books_response = [BookResponse.model_validate(book) for book in books]
   return books_response
+
+def create(book_data: BookCreate) -> BookResponse:
+  book_repository = BookRepository()
+  book = Book(**book_data.model_dump())
+  book_repository.insert(book)
+  return BookResponse.model_validate(book)
