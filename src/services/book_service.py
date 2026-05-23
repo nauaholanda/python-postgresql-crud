943dc1d94880e70168d3e5961c6de03b34730a84
Nauã Holanda <nauasholanda@gmail.com>
2026-05-23 19:40:25 -0300

    feat: add update book function to book service

--- src/services/book_service.py
+++ src/services/book_service.py
@@ -1,5 +1,5 @@
 from repositories import BookRepository
-from schemas.book import BookResponse, BookCreate
+from schemas.book import BookResponse, BookCreate, BookUpdate
 from entities import Book
 
 def get_all() -> list[BookResponse]:
@@ -12,4 +12,10 @@ def create(book_data: BookCreate) -> BookResponse:
   book_repository = BookRepository()
   book = Book(**book_data.model_dump())
   book_repository.insert(book)
+  return BookResponse.model_validate(book)
+
+def update(book_id: int, book_data: BookUpdate) -> BookResponse:
+  book_repository = BookRepository()
+  update_data = book_data.model_dump(exclude_unset=True)
+  book = book_repository.update(book_id, update_data)
   return BookResponse.model_validate(book)
