4d5a31c948e68be5d189b2921c86558c0baf5219
Nauã Holanda <nauasholanda@gmail.com>
2026-05-23 19:10:29 -0300

    feat: add insert book endpoint

--- src/api/books.py
+++ src/api/books.py
@@ -1,10 +1,14 @@
 from fastapi import APIRouter
 
 from services import book_service
-from schemas.book import BookResponse
+from schemas.book import BookResponse, BookCreate
 
 router = APIRouter(prefix="/books")
 
 @router.get("/", response_model=list[BookResponse])
 def get_all_books():
   return book_service.get_all()
+
+@router.post("/", response_model=BookResponse)
+def insert_book(book_data: BookCreate):
+  return book_service.create(book_data)
