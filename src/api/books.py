229b2617ffeaf2cc922f8329195271bec5ccaca0
Nauã Holanda <nauasholanda@gmail.com>
2026-05-23 19:41:15 -0300

    feat: create update book endpoint

--- src/api/books.py
+++ src/api/books.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 from services import book_service
-from schemas.book import BookResponse, BookCreate
+from schemas.book import BookResponse, BookCreate, BookUpdate
+from exceptions import BookNotFoundException
 
 router = APIRouter(prefix="/books")
 
@@ -12,3 +13,10 @@ def get_all_books():
 @router.post("/", response_model=BookResponse, status_code=201)
 def insert_book(book_data: BookCreate):
   return book_service.create(book_data)
+
+@router.put("/{book_id}", response_model=BookResponse)
+def update_book(book_id: int, book_data: BookUpdate):
+  try:
+    return book_service.update(book_id, book_data)
+  except BookNotFoundException as error:
+    raise HTTPException(status_code=404, detail=str(error))
