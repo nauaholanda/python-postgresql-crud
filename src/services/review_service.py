0d28e49c28d1440de580edb5d90ed28d4a72d1ac
Nauã Holanda <nauasholanda@gmail.com>
2026-05-24 12:12:30 -0300

    feat: create create review function in review service

--- src/services/review_service.py
+++ src/services/review_service.py
@@ -1,6 +1,7 @@
-from schemas.review import ReviewResponse
+from schemas.review import ReviewResponse, ReviewCreate
 from repositories import ReviewRepository, BookRepository
 from exceptions import BookNotFoundException
+from entities import Review
 
 def get_all_by_book_id(book_id: int) -> list[ReviewResponse]:
   validate_book_exists(book_id)
@@ -8,6 +9,13 @@ def get_all_by_book_id(book_id: int) -> list[ReviewResponse]:
   book_reviews = review_repository.find_all_by_book_id(book_id)
   reviews_response = [ReviewResponse.model_validate(review) for review in book_reviews]
   return reviews_response
+
+def create(book_id: int, review_data: ReviewCreate) -> ReviewResponse:
+  validate_book_exists(book_id)
+  review = Review(**review_data.model_dump(), book_id=book_id)
+  review_repository = ReviewRepository()
+  review_repository.insert(review)
+  return ReviewResponse.model_validate(review)
 
 def validate_book_exists(book_id: int) -> None:
   book_repository = BookRepository()
