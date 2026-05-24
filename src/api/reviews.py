7b1db4274b3c16616a0a907ba1b1bd4ed620fa45
Nauã Holanda <nauasholanda@gmail.com>
2026-05-24 12:12:53 -0300

    feat: add create review endpoint

--- src/api/reviews.py
+++ src/api/reviews.py
@@ -1,10 +1,14 @@
 from fastapi import APIRouter
 
 from services import review_service
-from schemas.review import ReviewResponse
+from schemas.review import ReviewResponse, ReviewCreate
 
 router = APIRouter(prefix="/books/{book_id}/reviews")
 
 @router.get("/", response_model=list[ReviewResponse])
 def get_all_reviews_by_book_id(book_id: int):
   return review_service.get_all_by_book_id(book_id)
+
+@router.post("/", response_model=ReviewResponse)
+def insert_review_to_a_book(book_id: int, review_data: ReviewCreate):
+  return review_service.create(book_id, review_data)
