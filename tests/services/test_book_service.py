e73b2bb081fa724dc7021a84116b9aa5faee5cd4
Nauã Holanda <nauasholanda@gmail.com>
2026-05-25 21:51:57 -0300

    test: add test case to get all books in book service

---
+++ tests/services/test_book_service.py
@@ -0,0 +1,28 @@
+from unittest.mock import patch, MagicMock, call
+import pytest
+
+from entities import Book
+from services import book_service
+
+@pytest.fixture
+def mock_book_repository():
+  book_repository = MagicMock()
+  with patch("services.book_service.BookRepository") as MockBookRepository:
+    MockBookRepository.return_value = book_repository
+    yield book_repository
+
+class TestGetAllBooks:
+  def test_get_all_books_successfully(self, mock_book_repository):
+    db_books = [MagicMock(spec=Book), MagicMock(spec=Book)]
+    parsed_books = [MagicMock(), MagicMock()]
+
+    mock_book_repository.select.return_value = db_books
+
+    with patch("services.book_service.BookResponse") as MockBookResponse:
+      MockBookResponse.model_validate.side_effect = parsed_books
+
+      response = book_service.get_all()
+
+    assert response == parsed_books
+    mock_book_repository.select.assert_called_once()
+    MockBookResponse.model_validate.assert_has_calls([call(b) for b in db_books])
