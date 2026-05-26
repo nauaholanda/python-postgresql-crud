702e13a0ace2e55bffa36b1963703cf1ac3f1b1d
Nauã Holanda <nauasholanda@gmail.com>
2026-05-25 21:54:49 -0300

    test: add test case to create book in book service

--- tests/services/test_book_service.py
+++ tests/services/test_book_service.py
@@ -3,6 +3,7 @@ import pytest
 
 from entities import Book
 from services import book_service
+from schemas.book import BookCreate
 
 @pytest.fixture
 def mock_book_repository():
@@ -26,3 +27,20 @@ class TestGetAllBooks:
     assert response == parsed_books
     mock_book_repository.select.assert_called_once()
     MockBookResponse.model_validate.assert_has_calls([call(b) for b in db_books])
+
+class TestCreateBook:
+  def test_create_book_successfully(self, mock_book_repository):
+    book_data = BookCreate(title="Book title", author="Author name", rating=10)
+    parsed_book = MagicMock()
+
+    with patch("services.book_service.Book") as MockBook, \
+      patch("services.book_service.BookResponse") as MockBookResponse:
+
+      MockBookResponse.model_validate.return_value = parsed_book
+
+      response = book_service.create(book_data)
+
+    assert response == parsed_book
+    MockBook.assert_called_once_with(**book_data.model_dump())
+    mock_book_repository.insert.assert_called_once_with(MockBook.return_value)
+    MockBookResponse.model_validate.assert_called_once_with(MockBook.return_value)
